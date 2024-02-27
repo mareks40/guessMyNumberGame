890808e649571730b076ffae62608eb081e8992e
Marek Sulikowski <mareksulikowski@gmail.com>
2024-02-27 22:55:45 +0100

    [writing code]: When the player wins

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ document.querySelector(".check").addEventListener("click", function () {
   // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+
+    // When the player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "✅ Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#106da3";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
 });
