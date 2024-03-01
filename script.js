c9568f2d04c168223cf3dfe2b359c703b183de79
Marek Sulikowski <mareksulikowski@gmail.com>
2024-03-01 20:30:10 +0100

    [refactor code]: zmiana kodu wiadomości

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No number!";
+    //document.querySelector(".message").textContent = "⛔ No number!";
+    displayMessage("⛔ No number!");
 
     // When the player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "✅ Correct number!";
+    //document.querySelector(".message").textContent = "✅ Correct number!";
+    displayMessage("✅ Correct number!");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#106da3";
     document.querySelector(".number").style.width = "30rem";
@@ -27,12 +33,14 @@ document.querySelector(".check").addEventListener("click", function () {
     // When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent =
-        guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+      /*document.querySelector(".message").textContent =
+        guess > secretNumber ? "📈 Too high!" : "📉 Too low!";*/
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "💣 You lost the game!";
+      //document.querySelector(".message").textContent = "💣 You lost the game!";
+      displayMessage("💣 You lost the game!");
       document.querySelector(".score").textContent = 0;
     }
   }
